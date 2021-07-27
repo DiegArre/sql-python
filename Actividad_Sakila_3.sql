ebc227667198359952bddf8f67a7188b6d823a6f
DiegArre <diego.arredo18@gmail.com>
2021-07-27 19:45:24 -0400

    Se termina actividad sakila

--- Actividad_Sakila_3.sql
+++ Actividad_Sakila_3.sql
@@ -25,7 +25,7 @@ SELECT actor.actor_id, CONCAT(actor.first_name," ", actor.last_name) as actor_na
 FROM actor
 JOIN film_actor ON film_actor.actor_id = actor.actor_id
 JOIN film ON film.film_id = film_actor.film_id
-WHERE actor.actor_id = 5;
+WHERE actor.actor_id = 5; 
 
 /*4. ¿Qué consulta ejecutaría para obtener todos los clientes en store_id = 1 y dentro de 
 estas ciudades (1, 42, 312 y 459)? Su consulta debe devolver el nombre, apellido, correo 
@@ -39,10 +39,46 @@ OR city.city_id = 42
 OR city.city_id = 312
 OR city.city_id = 459
 AND customer.store_id = 1
-ORDER BY store_id
+ORDER BY store_id;
 
 /*5. ¿Qué consulta realizarías para obtener todas las películas con una "calificación = G" 
 y "característica especial = detrás de escena", unidas por actor_id = 15? Su consulta debe 
 devolver el título de la película, la descripción, el año de lanzamiento, la calificación y 
 la función especial. Sugerencia: puede usar la función LIKE para obtener la parte 'detrás de escena'.*/
 
+SELECT * FROM film
+JOIN film_actor ON film_actor.film_id = film.film_id
+JOIN actor ON actor.actor_id = film_actor.actor_id
+WHERE film.rating = "G"
+AND film.special_features LIKE "%Behind the Scenes%"
+AND actor.actor_id = 15;
+
+/*6. ¿Qué consulta harías para obtener todos los actores que se unieron en el film_id = 369? 
+Su consulta debe devolver film_id, title, actor_id y actor_name.*/
+
+SELECT film.film_id , film.title, actor.actor_id, CONCAT(actor.first_name, " ", actor.last_name) as actor_name FROM film
+JOIN film_actor ON film_actor.film_id = film.film_id
+JOIN actor ON actor.actor_id = film_actor.actor_id
+WHERE film.film_id = 369;
+
+/*7. ¿Qué consulta harías para obtener todas las películas dramáticas con una tarifa de alquiler 
+de 2.99? Su consulta debe devolver el título de la película, la descripción, el año de estreno, 
+la calificación, las características especiales y el género (categoría).*/
+
+SELECT film.film_id, film.title, film.description, film.release_year, film.rating, film.special_features, film.rental_rate ,category.name  FROM film
+JOIN film_category ON film_category.film_id = film.film_id
+JOIN category ON category.category_id = film_category.category_id
+WHERE film.rental_rate = 2.99 AND category.name = "Drama";
+
+/*¿Qué consulta harías para obtener todas las películas de acción a las que se une SANDRA KILMER? 
+Su consulta debe devolver el título de la película, la descripción, el año de estreno, la calificación, 
+las características especiales, el género (categoría) y el nombre y apellido del actor.*/
+
+SELECT film.title, film.description, film.release_year, film.rating, film.special_features, category.name, 
+CONCAT(actor.first_name, " ", actor.last_name) as actor_name  
+FROM film
+JOIN film_category ON film_category.film_id = film.film_id
+JOIN category ON category.category_id = film_category.category_id
+JOIN film_actor ON film_actor.film_id = film.film_id
+JOIN actor ON actor.actor_id = film_actor.actor_id
+WHERE actor.first_name = "SANDRA" AND actor.last_name = "KILMER" AND category.name = "Action";
